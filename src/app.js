6b795c6d2df49afdcf53b6d3d00ea15dbf1cdfb6
DannyRidz <danny.ridz93@gmail.com>
2026-09-21 07:47:14 +0800

    feat: implement basic Express server for file uploading

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("src/public"));
+
+app.get("/", (req, res) => {
+  res.send("File Uploader is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`File Uploader is running at http://localhost:${PORT}`);
+});
